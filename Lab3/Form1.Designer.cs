dd29a02eeef2a5971b2b8a8500e22ecf553cafea
ndargi@bu.edu <NickDargi@Nicks-MacBook-Pro-2.local>
2016-10-13 17:15:14 -0400

    Commit

---
+++ Lab3/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Clear = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(65, 88);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(155, 71);
+            this.Clear.TabIndex = 0;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(826, 42);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clearToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(64, 38);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(269, 38);
+            this.clearToolStripMenuItem.Text = "Clear";
+            this.clearToolStripMenuItem.Click += new System.EventHandler(this.clearToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(826, 797);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Lab3-Nicholas Dargi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
+    }
+}
+
---
+++ Lab3/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Windows.Forms;
+
+namespace Lab3
+{
+    public partial class Form1 : Form
+    {
+        private List<ClickPoints> coordinates = new List<ClickPoints>();
+        const int WIDTH = 20;
+        const int HEIGHT = 20;
+        private class ClickPoints
+        {
+            public Point p;
+            //if ture it is black if false it is red
+            public bool black;
+           
+            public ClickPoints(Point mypoint,bool isblack)
+            {
+                p = mypoint;
+                black = isblack;
+            }
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                //Add new Click to the Array
+                Point p = new Point(e.X, e.Y);
+                // Add point and Black status to the ClickPoints Class, then add class to List
+                ClickPoints myClick = new ClickPoints(p,true);
+                coordinates.Add(myClick);
+                Invalidate();
+            }
+            if (e.Button == MouseButtons.Right)
+            {
+                //Loop from last element to first element in case an element needs to be removed
+                for (int i = (coordinates.Count)-1; i > -1; i--)
+                {                   
+                    ClickPoints Clicks = coordinates[i];
+                    //check if the click is within the point
+                    if (Clicks.p.X - WIDTH <= e.X && Clicks.p.X + WIDTH >= e.X && Clicks.p.Y - HEIGHT <= e.Y && Clicks.p.Y + HEIGHT >= e.Y)
+                    {
+                        //If it is black turn it to red and modify the list, else its already red so remove it from list
+                        if (Clicks.black == true)
+                        {
+                            Clicks.black = false;
+                            coordinates[i] = Clicks;
+                        }
+                        else
+                        {                           
+                            coordinates.RemoveAt(i);                           
+                        }
+                        
+                    }
+                }
+                
+                Invalidate();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            
+            Graphics g = e.Graphics;
+
+            
+            foreach (ClickPoints Clicks in coordinates)
+            {
+                //Set default brush to red, if CLicks.black is TRUE change it to black
+                Brush myBrush = Brushes.Red;
+                if (Clicks.black)
+                {
+                    myBrush = Brushes.Black;
+                }
+                g.FillEllipse(myBrush, Clicks.p.X - WIDTH / 2, Clicks.p.Y - WIDTH / 2, WIDTH, HEIGHT);
+                //Commented out code from Lab 2 per request of Prof. Skinner
+                //    string placement = "(" + p.X + ", " + p.Y + ")";
+                //    g.DrawString(placement, Font, Brushes.Black, p.X + 20, p.Y - 7);
+
+            }
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            coordinates.Clear();
+            Invalidate();
+        }
+
+        private void clearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            coordinates.Clear();
+            Invalidate();
+        }
+    }
+}
